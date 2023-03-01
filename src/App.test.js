560e8ed5476d33b4dbab08f7e8f7ed76ec578581
Berkay Gülyaz <berkay.gulyaz@emlakjet.com>
2023-03-01 16:36:50 +0300

    I created my first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render App component without creashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  const element = screen.getByText('Modern Testing');
+  expect(element).toBeInTheDocument();;
+})
